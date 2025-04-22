7a61c9dbbe6092f10777186c8e64697e2dc74b13
Thomas YIu <tom.tyiu@gmail.com>
2025-04-22 14:55:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 # Show title and description.
 st.title("📄 Document question answering")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
+    "Upload a document below and ask a question about it – GPT will search will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
